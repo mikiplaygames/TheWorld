1ade1bf201f33bf0dd61e757eed5aa8a1d141412
Doc <82221725+ThatDoc@users.noreply.github.com>
2022-11-20 00:09:41 +0100

    Added mouse and snail
    no code

---
+++ Animals/Mouse.py
@@ -0,0 +1,9 @@
+import Animal
+import random
+
+
+class MOUSE(Animal.Animal):
+    strength = 2
+    defaultLifeSpan = 40
+    lifeSpan = defaultLifeSpan
+    moveChance = 90
---
+++ Animals/Snail.py
@@ -0,0 +1,9 @@
+import Animal
+import random
+
+
+class SNAIL(Animal.Animal):
+    strength = 1
+    defaultLifeSpan = 90
+    lifeSpan = defaultLifeSpan
+    moveChance = 10
